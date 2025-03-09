2e9f7ab19d8de27e7b30eac0e798e91e1b51588a
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2025-03-09 20:08:00 -0300

    feat#86a6zfz8v/Integração Google Cloud Storage
    Foram criadas classes para integrar o Google Cloud Storage

--- src/main/java/com/reportai/reportaiserver/controller/ImagemController.java
+++ src/main/java/com/reportai/reportaiserver/controller/ImagemController.java
@@ -1,11 +1,22 @@
 package com.reportai.reportaiserver.controller;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.reportai.reportaiserver.model.Imagem;
 import com.reportai.reportaiserver.service.ImagemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/imagens")
@@ -14,6 +25,12 @@ public class ImagemController {
 
    @Autowired
    private ImagemService service;
+
+   @Value("${gcs.bucket-name}")
+   private String bucketName;
+
+   @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
+   private String googleApplicationCredentials;
 
    @PostMapping
    public ResponseEntity<?> salvar(@RequestBody Imagem imagem) {
@@ -53,5 +70,24 @@ public class ImagemController {
       }
    }
 
+   @PostMapping("/upload")
+   public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+      Storage storage = StorageOptions
+              .newBuilder()
+              .setCredentials(GoogleCredentials.fromStream(new FileInputStream(googleApplicationCredentials)))
+              .setProjectId("reportai-453222").build().getService();
+
+
+      String fileName = UUID.randomUUID().toString();
+      BlobId blobId = BlobId.of(bucketName, fileName);
+      BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+              .setContentType(file.getContentType())
+              .build();
+
+      storage.create(blobInfo, file.getBytes());
+      return "https://storage.googleapis.com/" + bucketName + "/" + fileName;
+
+   }
 
 }
